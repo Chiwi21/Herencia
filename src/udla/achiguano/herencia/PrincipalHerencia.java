be52ffff9f3b400e9c8de44fdb8393906088d212
Chiwi21 <alfredochiguano85@gmail.com>
2024-11-11 10:35:26 -0500

    c

--- src/udla/achiguano/herencia/PrincipalHerencia.java
+++ src/udla/achiguano/herencia/PrincipalHerencia.java
@@ -3,10 +3,10 @@ package udla.achiguano.herencia;
 public class PrincipalHerencia  {
     Persona persona = new Persona();
     //Por defecto le crea el constructor
-    Alumno alumno = new Alumno;
+    Alumno alumno = new Alumno();
     Profesor profesor = new Profesor();
 
     //Forma no tan recomendada: Es cambiar a protected los atributos para cambiar los valores de los atributos en el main, pero SOLO saldrán los atributos de la clase padre.
     // alumno.apellido    (No es bueno por el principio de ocultamiento)
-    Persona alumno = new Alumno();   //Esto es posible por el extend, instancia alumno en la clase padre Persona
+    //Persona alumno = new Alumno();   //Esto es posible por el extend, instancia alumno en la clase padre Persona
 }
